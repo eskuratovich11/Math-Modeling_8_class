9dc302ee03d729411ad366d83f86c9f8df47d21c
eskuratovich11 <56356789+eskuratovich11@users.noreply.github.com>
2019-10-09 19:07:11 +0300

    Лаба1

---
+++ laba_1.py
@@ -0,0 +1,42 @@
+print("-------Задание№1-------")
+print("Hello, world!")
+print("Маша+Петя=Любовь")
+print("х=3+4")
+х=3+4
+print("х=",х)
+print()
+print("-------Задание№2-------")
+x="Hello, world!"
+print("х= Hello, world!")
+print(type(x))
+x=3+4
+print("х=3+4")
+print(type(x))
+x=3/4
+print("х=3/4")
+print(type(x))
+x=[1,2,5,10,100]
+print("х=[1,2,5,10,100]")
+print(type(x))
+print()
+print("-------Задание№3-------")
+x=3
+y=(x**2)**0.5/(x**3+3/x)*(4*x**7-x**5)+80*((27*x**4+12*x**3-5*x**2+10)**0.5)
+print(y)
+x=3
+y=(3%2+(16.7*4.32)//1)/(14.5+31%12-(х**3.4)//1)
+print(y)
+print()
+print("-------Задание№4-------")
+a=[1,5,"Good","Bad"]
+b=[9,"Blue","Red",11]
+x=a[1]+b[3]
+print(x)
+x=a[2]+b[2]
+print(x)
+x=a[0]*b[0]
+print(x)
+x=a[1]**b[3]
+print(x)
+x=a+b
+print(x)
