6744ff06752e0bfac70ef5f647ddea32d5dd53d9
eskuratovich11 <56356789+eskuratovich11@users.noreply.github.com>
2019-10-16 18:04:09 +0200

    календарь

---
+++ календарь.py
@@ -0,0 +1,22 @@
+mesac=6
+a=(14-mesac)//12
+god1=1992
+god=god1-a
+mesac1=mesac+(12*a)-2
+chislo=15
+dennedeli=(chislo+(31*mesac1)//12+god+god//4-god//100+god//400)%7
+if dennedeli==0:
+    x='воскресенье'
+elif dennedeli==1:
+    x='понедельник'
+elif dennedeli==2:
+    x='вторник'
+elif dennedeli==3:
+    x='среда'
+elif dennedeli==4:
+    x='четверг'
+elif dennedeli==5:
+    x='пятница'
+elif dennedeli==6:
+    x='суббота'
+print(x)
